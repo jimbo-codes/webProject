7805f43b7aa5ef26456d8a2025a0ee363b4a2831
James <jrburns96@aol.com>
2021-10-19 19:26:54 -0400

    started HTML for form, added local css into html file

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+//const h2 = document.createElement("h2");
+//h2.textContent = "This content added by JavaScript";
+//document.querySelector("body").appendChild(h2);
+
+
+
+// idea - I want to create a basic form fillout website (contact info to sign for mailing list)
+
+// Then I want to programatically send an email on each form submission.
+// use a click on button button to do the thing: 
+//A click on a button button does... nothing! That sounds silly, but it's amazingly useful 
+// for building custom buttons — you can define their chosen functionality with JavaScript.
+
+// Also then take the information from the form and have the person be an object / marketing item.
