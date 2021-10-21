323ab6d747a89fe40c3b54865d992bbae226c6ff
James Burns <jrburns96@aol.com>
2021-10-20 20:17:26 -0400

    convoluted loop building

--- script.js
+++ script.js
@@ -8,15 +8,92 @@
 //document.addEventListener('DOMContentLoaded', init);
 const element = document.getElementById("#name");
 
-const submit = function(){
-    const reqmark = document.createElement("span");
-    const text = document.createTextNode("* Required");
-    reqmark.appendChild(text);
-  // create the *required* element, and insert them where appropriate to right of the form - append to "NAME"
-  
-  document.getElementById("start").appendChild(reqmark);
-//  document.body.ul.append(reqmark);
+const checker = function(){
+//const inputFeilds = document.querySelectorAll("input");
+//const validInputs = Array.from(inputFeilds).filter( input => input.value !== "");
+//console.log(validInputs) //[array with valid inputs]
+
+// get element by class, log all into the array and then run a for in loop to aappend * required
+var test = document.getElementsByClassName("check")
+
+// this returns a nodelist objectS - you now need to figure out how you can loop through these objs to test if empty
+let inputs = Array.from(test); // converts nodelist (returned by getElementsByClassName) into an array
+
+/*
+for (var i = 0; i < inputs.length; ++i) {
+    if (x[i].value == "") {
+        ++empty;
+        // tell it to pull array value, use the answer from that to specify the input there
     }
+}
+*/
+
+//AFTER THIS LOOP GOES - Loop a check to make sure that we SHOULD be adding text
+
+// This loops through and sets
+console.log(inputs);
+for( let i =0; i < inputs.length; i++){
+const flag = document.createElement("span");
+const text = document.createTextNode("* Required");
+    flag.appendChild(text);
+    let x = inputs[i];
+  x.appendChild(flag);
+// Needs to now test each of these, and an exception handler
+// for the checkbox
+}
+
+//let id = document.[inputs[i]]
+
+
+
+/*
+if (id.hasChildNodes()) {
+    let children= id.children;
+    console.log(children[1]);
+    for (let i=0; i < children.length; i++){
+        console.log(i);
+        if (children[i].hasChildren){
+            console.log(`${children[i]}`+"it still has kids")
+        }
+        else{
+            console.log("This was the last kid!")
+        }
+        */
+        // Nest an if inside this to test if it has a child, if no, do the below.
+ /*    const flag = document.createElement("span");
+    const text = document.createTextNode("* Required");
+        flag.appendChild(text);        
+      document.getElementById("start").appendChild(reqmark);
+
+}
+else{
+    console.log("errr");
+*/
+}
+const submit = function(){
+// if(truth){
+    checker();
+//    const reqmark = document.createElement("span");
+ //   const text = document.createTextNode("* Required");
+  //  reqmark.appendChild(text);  
+  // document.getElementById("start").appendChild(reqmark);
+  
+  
+  
+  
+  // Make this a loop where it goes through by each element - recursive function?
+  // call the "checker" function for each element, and have it run through the above process
+
+  //}
+  
+
+
+/* else{
+    alert("throwing an error");
+} */
+}
+
+
 // This has to go AFTER you've defined the function.
     const button = document.getElementById('button');
     button.addEventListener('click', submit);      
